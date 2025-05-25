ce3d92010a184459395c8e758dc0788b3d185bc3
sansquer77 <cristiano_gaspar@outlook.com>
2025-05-25 15:10:32 -0300

    Update BF1alpha.py

--- BF1alpha.py
+++ BF1alpha.py
@@ -12,6 +12,88 @@ JWT_SECRET = 'sua_chave_secreta_supersegura'
 JWT_EXP_MINUTES = 120
 
 REGULAMENTO = """
+REGULAMENTO BF1-2025
+
+O BF1-2025 terá início, oficialmente, em 16 de março, no dia do GP da Austrália e término em 07 de dezembro, quando será disputado o último GP, o de Abu Dhabi.
+
+Inscrições para o BF1 estão liberadas a partir de qualquer etapa.
+A inscrição é de R$200,00 a ser pago no ato da inscrição via PIX.
+Em caso de desistência da participação durante o campeonato a taxa de inscrição não será devolvida.
+Cabe ressaltar, que a pontuação do novo participante será 80% da pontuação do participante mais mal colocado no bolão no momento da inscrição e terá 0 pontos na aposta de campeão, caso ocorra após o início do campeonato.
+
+As apostas dos participantes devem ser efetuadas até o horário programado da corrida e compartilhadas via formulário padrão que está no grupo do WhatsApp. Fica facultado ao participante a geração de um print da tela do aplicativo com a aposta e o horário da mensagem para futuras validações, mas cabe ressaltar que a ferramenta possui um Time Stamp do horário de envio das mensagens.
+
+a. O participante pode enviar quantas apostas quiser até o horário limite, sendo válida a última enviada.
+b. Apostas registradas após o horário da largada, por exemplo 09:01 sendo a corrida às 09h serão desconsideradas.
+c. Os horários das corridas deste ano são:
+
+Grande Prêmio Data Horário
+
+1  Grande Prêmio da Austrália           16 de março         01:00
+2  Grande Prêmio da China Sprint        22 de março         00:00
+3  Grande Prêmio da China               23 de março         04:00
+4  Grande Prêmio do Japão               6 de abril          02:00
+5  Grande Prêmio do Bahrain             13 de abril         12:00
+6  Grande Prêmio da Arábia Saudita      20 de abril         14:00
+7  Grande Prêmio de Miami Sprint        3 de maio           13:00
+8  Grande Prêmio de Miami               4 de maio           17:00
+9  Grande Prêmio da Emília-Romanha      18 de maio          10:00
+10 Grande Prêmio de Mônaco              25 de maio          10:00
+11 Grande Prêmio da Espanha             1 de junho          10:00
+12 Grande Prêmio do Canadá              15 de junho         15:00
+13 Grande Prêmio da Áustria             29 de junho         10:00
+14 Grande Prêmio da Grã-Bretanha        6 de julho          11:00
+15 Grande Prêmio da Bélgica Sprint      26 de julho         07:00
+16 Grande Prêmio da Bélgica             27 de julho         10:00
+17 Grande Prêmio da Hungria             3 de agosto         10:00
+18 Grande Prêmio dos Países Baixos      31 de agosto        10:00
+19 Grande Prêmio da Itália              7 de setembro       10:00
+20 Grande Prêmio do Azerbaijão          21 de setembro      08:00
+21 Grande Prêmio de Singapura           5 de outubro        09:00
+22 Grande Prêmio dos EUA Sprint         18 de outubro       14:00
+23 Grande Prêmio dos EUA                19 de outubro       16:00
+24 Grande Prêmio da Cidade do México    26 de outubro       17:00
+25 Grande Prêmio de São Paulo Sprint    8 de novembro       11:00
+26 Grande Prêmio de São Paulo           9 de novembro       14:00
+27 Grande Prêmio de Las Vegas           22 de novembro      01:00
+28 Grande Prêmio do Catar Sprint        29 de novembro      11:00
+29 Grande Prêmio do Catar               30 de novembro      13:00
+30 Grande Prêmio de Abu Dhabi           7 de dezembro       10:00
+
+O participante que não efetuar a sua aposta ATÉ O PRAZO DEFINIDO DO ITEM-3, irá concorrer com a mesma aposta da última corrida.
+Quando se tratar da primeira vez que a aposta não for feita, e apenas neste caso, será computado 100% dos pontos.
+Caso o apostador não aposte na primeira corrida do campeonato, como não haverá base para repetição da aposta a pontuação será 80% do pior pontuador para esta corrida e o benefício do item “a” deste tópico será mantido.
+Para o segundo atraso em diante os pontos sofrerão um desconto de 25%.
+
+Pontuação
+
+Cada participante deve indicar o campeão e o vice do campeonato de pilotos e a equipe vencedora do campeonato de construtores ANTES do início da primeira prova do ano em formulário específico.
+A pontuação será 150 pontos se acertar o campeão, 100 se acertar o vice, 80 acertando equipe – Que serão somados à pontuação ao final do campeonato.
+
+Cada participante possui 15 (quinze) fichas para serem apostadas a cada corrida da seguinte maneira:
+A aposta deve conter no mínimo 3 pilotos de equipes diferentes (Apostou no Hamilton, não pode apostar no Leclerc por ex.)
+Sem limite de ficha por piloto, vale 13 / 1 / 1, desde que respeitada a regra acima.
+As corridas Sprint seguem a mesma regra, sendo consideradas provas válidas para a pontuação.
+Deve ser indicado o piloto que irá chegar em 11º lugar em todas as provas e em caso de acerto será computado 25 pontos.
+
+A pontuação do participante será a multiplicação das fichas apostadas em cada piloto pelo número de pontos que ele obteve na prova (fichas x pontos) + pontuação do 11º lugar.
+As apostas serão lançadas na planilha de controle que está hospedada no OneDrive, sendo que o placar atualizado será publicado na página do grupo e no WhatsApp após as corridas.
+
+Critérios de Desempate
+
+Caso haja empate de pontos na classificação final, as posições serão definidas pelos seguintes critérios, na ordem:
+Quem tiver apostado antes mais vezes no ano
+Quem mais vezes acertou o 11º lugar
+Quem acertou o campeão
+Quem acertou a equipe campeã
+Quem acertou o vice
+
+Forma de pagamento e premiação
+
+A premiação será um voucher de 50% do fundo arrecadado das inscrições para o primeiro colocado, 30% para o segundo e 20% para o terceiro gastarem nas bebidas de sua escolha a serem adquiridas após a definição dos vencedores e escolha dos prêmios.
+
+A premiação será realizada em um Happy-Hour a ser agendado entre os participantes em data e local a serem definidos posteriormente ao final do campeonato.
+"""
 
 def ler_regulamento(arquivo="regulamento.txt"):
     try:
