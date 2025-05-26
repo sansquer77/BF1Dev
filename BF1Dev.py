64d4f8dd9eaeb241432f45d6c5fb823b58867d30
sansquer77 <cristiano_gaspar@outlook.com>
2025-05-26 11:57:03 -0300

    Update BF1Dev.py

--- BF1Dev.py
+++ BF1Dev.py
@@ -116,15 +116,15 @@ def init_db():
         VALUES (?, ?, ?, ?, ?, ?)''',
         ('Password', 'master@bolao.com', senha_hash, 'master', 'Ativo', 0))
     c.execute('''CREATE TABLE IF NOT EXISTS pilotos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT,
-    equipe TEXT,
-    status TEXT DEFAULT 'Ativo')''')
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT,
+        equipe TEXT,
+        status TEXT DEFAULT 'Ativo')''')
     c.execute('''CREATE TABLE IF NOT EXISTS provas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT,
-    data TEXT,
-    status TEXT DEFAULT 'Ativo')''')
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT,
+        data TEXT,
+        status TEXT DEFAULT 'Ativo')''')
     c.execute('''CREATE TABLE IF NOT EXISTS apostas (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         usuario_id INTEGER,
