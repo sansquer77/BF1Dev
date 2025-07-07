e4da593232f8bc9824044ef41a6cc7e85e54b2d3
sansquer77 <cristiano_gaspar@outlook.com>
2025-07-07 17:03:28 -0300

    Update BF1Dev.py

--- BF1Dev.py
+++ BF1Dev.py
@@ -625,7 +625,7 @@ def salvar_classificacao_prova(prova_id, df_classificacao):
             (prova_id, usuario_id, posicao, pontos, data_registro)
             VALUES (?, ?, ?, ?, ?)
         ''', (prova_id, usuario_id, posicao, pontos, data_registro))
-        salvar_classificacao_prova(prova_id_atual, df_classificacao)
+        salvar_classificacao_prova(prova_id, df_classificacao)
     conn.commit()
     conn.close()
 
